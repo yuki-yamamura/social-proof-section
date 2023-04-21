62f0085277e63a3ca95cc88d04808fdc49f584b9
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-04-22 08:45:59 +0900

    add font and colors to Tailwind CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['**/*.html'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['League Spartan', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        veryDarkMagenta: 'hsl(300, 43%, 22%)',
+        softPink: 'hsl(333, 80%, 67%)',
+        darkGrayishMagenta: 'hsl(303, 10%, 53%)',
+        lightGrayishMagenta: 'hsl(300, 24%, 96%)',
+      },
+    },
   },
   plugins: [],
 };
